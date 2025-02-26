21f2d108f9125611bdb495af7eba1d17b4f0499a
Jia <49358172+craftslab@users.noreply.github.com>
2025-02-26 07:47:47 +0000

    feat(templated_tests): update test script

--- templated_tests/java_gradle/run_test.sh
+++ templated_tests/java_gradle/run_test.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-cover-agent --source-file-path="src/main/java/com/davidparry/cover/SimpleMathOperations.java" --test-file-path="src/test/java/com/davidparry/cover/SimpleMathOperationsTest.java" --code-coverage-report-path="build/reports/jacoco/test/jacocoTestReport.csv" --test-command="./gradlew clean test jacocoTestReport" --test-command-dir=$(pwd) --coverage-type="jacoco" --desired-coverage=70 --max-iterations=1
+cover-agent \
+  --api-base="https://ark.cn-beijing.volces.com/api/v3" \
+  --api-key="key" \
+  --model="model" \
+  --source-file-path="src/main/java/com/davidparry/cover/SimpleMathOperations.java" \
+  --test-file-path="src/test/java/com/davidparry/cover/SimpleMathOperationsTest.java" \
+  --code-coverage-report-path="build/reports/jacoco/test/jacocoTestReport.csv" \
+  --test-command="./gradlew clean test jacocoTestReport" \
+  --test-command-dir=$(pwd) \
+  --coverage-type="jacoco" \
+  --desired-coverage=70 \
+  --max-iterations=1
